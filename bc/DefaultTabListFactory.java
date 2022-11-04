e8184792dcd1aa12e6e3a4955c68f22dca5785a0
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 20:07:50 +0000

    added bc stuffs

--- bc/DefaultTabListFactory.java
+++ bc/DefaultTabListFactory.java
@@ -5,7 +5,7 @@ package net.md_5.bungee.tab;
 // - add DefaultTabListFactory import
 // - add static member "DefaultTabListFactory tabListFactory" and initialize with default list factory object
 // - add public static method setTabListFactory that replaces the default object
-// - in function init() replace the object assigned to tabListHandler with tavbListFactory.createTabList(this)
+// - in function init() replace the object assigned to tabListHandler with tabListFactory.createTabList(this)
 
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
